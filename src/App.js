14b7c400ceb6dc8f3f8a7d928f7b72dafb8d098a
ahmadalmuhtar <ahmadalmuhtar@gmail.com>
2023-11-24 13:20:16 +0100

    created the first search button with input text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [forecastData, setForecastData] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      const [currentWeather, forecast] = await Promise.all([
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY&units=metric`),
+        axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=YOUR_API_KEY&units=metric`),
+      ]);
+      setWeatherData(currentWeather.data);
+      setForecastData(forecast.data.list);
+    } catch (error) {
+      console.error('Error fetching weather data: ', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather App</h1>
+      <input type="text" placeholder="Enter City" value={city} onChange={(e) => setCity(e.target.value)} />
+      <button onClick={handleSearch}>Search</button>
+      {
+        weatherData && (
+          <div>
+            <h2>{weatherData.name}</h2>
+            <p>Temperature: {weatherData.main.temp} °C</p>
+            <p>Weather: {weatherData.weather[0].description}</p>
+          </div>
+        )}
+      {forecastData && (
+        <div>
+          <h2>Five-Day Forecast</h2>
+          <div className="forecast-container">
+            {forecastData.map((forecast) => (
+              <div key={forecast.dt} className="forecast-item">
+                <p>{new Date(forecast.dt * 1000).toLocaleDateString()}</p>
+                <p>Temperature: {forecast.main.temp} °C</p>
+                <p>Weather: {forecast.weather[0].description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
